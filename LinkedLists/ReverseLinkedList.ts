4e49b73d6e3638b5a0e3522dd487fc040b6388b7
arturfil <arturfil@hotmail.com>
2022-03-29 23:00:13 -0400

    added solution and explanation of reverse linked list

--- LinkedLists/ReverseLinkedList.ts
+++ LinkedLists/ReverseLinkedList.ts
@@ -2,6 +2,37 @@ import ListNode from "./ListNode";
 
 export class ReverseLinkedList {
     reverseList(head: ListNode | null): ListNode | null {
+        let previous = null;
+        while(head != null) {
+            let current = head.next;
+            head.next = previous;
+            previous = head;
+            head = current;
+        }
         return head;
     }   
 }
+
+/*
+    TEST
+    const head = new ListNode(1);
+    const node2 = new ListNode(2);
+    const node3 = new ListNode(3);
+    const node4 = new ListNode(4);
+    const node5 = new ListNode(5);
+
+    head.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+
+    const rev = new ReverseLinkedList();
+    rev.reverseList(head);
+
+    EXPLANATION
+    - You essentially want to programatically change the direction of the arrows
+    - I.E. if linked list is 1 -> 2 -> 3; you want 1 <- 2 <- 3, where 3 is head now
+    - For that you have to do it programatically travelling one node ahead "current.next"
+    and pointing current.next to current where you also store the value of the "previous" node
+    and initially previous is null
+*/
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import { MajorityElement } from "./Arrays/MajorityElement";
 import { RotateImage } from "./Arrays/RotateImage";
 import { TwoSum } from "./Arrays/TwoSum";
+import ListNode from "./LinkedLists/ListNode";
+import { ReverseLinkedList } from "./LinkedLists/ReverseLinkedList";
 import { LongestSubstring } from "./Strings/LongestSubstring";
 import { UniqueEmails } from "./Strings/UniqueEmails";
 
@@ -8,9 +10,20 @@ import { UniqueEmails } from "./Strings/UniqueEmails";
 class Main {
 
     main() {
-        const test1 = "abcabcbb"
-        const lng = new LongestSubstring();
-        console.log(lng.lengthOfLongestSubstring(test1));
+        // [1,2,3,4,5]
+        const head = new ListNode(1);
+        const node2 = new ListNode(2);
+        const node3 = new ListNode(3);
+        const node4 = new ListNode(4);
+        const node5 = new ListNode(5);
+
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        const rev = new ReverseLinkedList();
+        rev.reverseList(head);
     }
 }
 
