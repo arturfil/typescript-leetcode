9c214fe4879ef074923cc552d2f422e7c92d3084
arturfil <arturfil@hotmail.com>
2022-06-07 19:11:22 -0400

    added refactoring

--- arrays/NextPermutation.ts
+++ arrays/NextPermutation.ts
@@ -1,9 +1,22 @@
 export class NextPermutation {
-    // [1,2,3] // 
-    // [3,2,1]
-    // [1,1,5]
+    nextPermutation2(nums: number[]):number[] | void {
+        if (nums.length <= 1) return;
+
+        let firstDesc = nums.length - 2;
+        while (firstDesc >= 0 && nums[firstDesc] >= nums[firstDesc + 1]) firstDesc--;
+        if (firstDesc === -1) return nums.reverse();
+
+        let toSwap = firstDesc + 1;
+        while (nums[toSwap + 1] > nums[firstDesc]) toSwap++;
+
+        [nums[firstDesc], nums[toSwap]] = [nums[toSwap],nums[firstDesc]]
+
+        for (let leftIdx = firstDesc + 1, rightIdx = nums.length - 1; leftIdx < rightIdx; leftIdx++, rightIdx--) {
+            [nums[leftIdx], nums[rightIdx]] = [nums[rightIdx],nums[leftIdx]]
+        }
+    }
+
     nextPermutation(nums: number[]): number[] {
-        
         for (let i = nums.length-2; i >= 0; i--) {
             let j = i+1;
 
@@ -22,6 +35,5 @@ export class NextPermutation {
         nums.sort((a, b) => a-b); // if max value, return the smallest value
         return nums;
     }
-
 
 }
